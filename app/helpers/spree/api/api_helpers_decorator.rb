1b85bab9860c172ef7c829884e4fe6341036a95d
Iqra Rastee <iqra.rastee@arkhitech.com>
2014-11-25 15:16:43 +0500

    added mpc and gtin to api

--- app/helpers/spree/api/api_helpers_decorator.rb
+++ app/helpers/spree/api/api_helpers_decorator.rb
@@ -1,9 +1,11 @@
 Spree::Api::ApiHelpers.class_eval do
-  def variant_attributes_with_upc
-    variant_attributes_without_upc << :upc
+  def variant_attributes_with_additional_attributes
+    variant_attributes_without_additional_attributes << :upc
+    variant_attributes_without_additional_attributes << :mpc
+    variant_attributes_without_additional_attributes << :gtin
   end
 
-  alias_method_chain :variant_attributes, :upc
+  alias_method_chain :variant_attributes, :additional_attributes
 
 #  class_variable_set(:@@variant_attributes, class_variable_get(:@@variant_attributes).push(:upc))
 end
